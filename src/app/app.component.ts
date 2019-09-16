7488e21f58a02250ba232e61140c1f87bd235ea3
Ognjen Marinkovic <ognjenmarinkovic369@gmail.com>
2019-09-16 19:40:42 +0200

    Fixed scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { routerTransition } from './routing.animation';
 
 @Component({
   selector: 'app-root',
-  animations: [ routerTransition ],
+  animations: [routerTransition],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {
+
+  }
   records = []
   getState(outlet) {
     return outlet.activatedRouteData.state
-    }
+  }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#2C5F2DFF';
+  }
 }
